0d8ba7c330b7d6f225cb38e18548d95008b52d00
Garrett,Einhorn <garrett.einhorn@kellogg.northwestern.edu>
2021-01-27 23:26:51 -0600

    console under each state

--- noober.js
+++ noober.js
@@ -38,6 +38,16 @@ if (ride.length == 1 || ride.length == 2 || ride.length == 3) {
   passenger1PickupAddressLine2 = passenger1.pickupLocation.city + " " + passenger1.pickupLocation.state + " " + passenger1.pickupLocation.zip
   passenger1DropoffAddressLine1 = passenger1.dropoffLocation.address
   passenger1DropoffAddressLine2 = passenger1.dropoffLocation.city + " " + passenger1.dropoffLocation.state + ' ' +passenger1.dropoffLocation.zip 
+
+  console.log(passenger1Name)
+  console.log(passenger1Phone)
+  console.log(passenger1NumberOfPassengers)
+  console.log(passenger1PickupAddressLine1)
+  console.log(passenger1PickupAddressLine2)
+  console.log(passenger1DropoffAddressLine1)
+  console.log(passenger1DropoffAddressLine2)
+
+
 } 
 else if (ride.length == 2 || ride.length == 3) {
   passenger2Name = passenger2.passengerDetails.first + ' ' + passenger2.passengerDetails.last
@@ -47,6 +57,23 @@ else if (ride.length == 2 || ride.length == 3) {
   passenger2PickupAddressLine2 = passenger2.pickupLocation.city + " " + passenger2.pickupLocation.state + " " + passenger2.pickupLocation.zip
   passenger2DropoffAddressLine1 = passenger2.dropoffLocation.address
   passenger2DropoffAddressLine2 = passenger2.dropoffLocation.city + " " + passenger2.dropoffLocation.state + ' ' +passenger2.dropoffLocation.zip 
+
+  console.log(passenger1Name)
+  console.log(passenger1Phone)
+  console.log(passenger1NumberOfPassengers)
+  console.log(passenger1PickupAddressLine1)
+  console.log(passenger1PickupAddressLine2)
+  console.log(passenger1DropoffAddressLine1)
+  console.log(passenger1DropoffAddressLine2)
+
+  console.log(passenger2Name)
+  console.log(passenger2Phone)
+  console.log(passenger2NumberOfPassengers)
+  console.log(passenger2PickupAddressLine1)
+  console.log(passenger2PickupAddressLine2)
+  console.log(passenger2DropoffAddressLine1)
+  console.log(passenger2DropoffAddressLine2)
+
 }
 else if (ride.length == 3) {
   passenger3Name = passenger3.passengerDetails.first + ' ' + passenger3.passengerDetails.last
@@ -56,6 +83,31 @@ else if (ride.length == 3) {
   passenger3PickupAddressLine2 = passenger3.pickupLocation.city + " " + passenger3.pickupLocation.state + " " + passenger3.pickupLocation.zip
   passenger3DropoffAddressLine1 = passenger3.dropoffLocation.address
   passenger3DropoffAddressLine2 = passenger3.dropoffLocation.city + " " + passenger3.dropoffLocation.state + ' ' +passenger3.dropoffLocation.zip 
+  
+  console.log(passenger1Name)
+  console.log(passenger1Phone)
+  console.log(passenger1NumberOfPassengers)
+  console.log(passenger1PickupAddressLine1)
+  console.log(passenger1PickupAddressLine2)
+  console.log(passenger1DropoffAddressLine1)
+  console.log(passenger1DropoffAddressLine2)
+  
+  console.log(passenger2Name)
+  console.log(passenger2Phone)
+  console.log(passenger2NumberOfPassengers)
+  console.log(passenger2PickupAddressLine1)
+  console.log(passenger2PickupAddressLine2)
+  console.log(passenger2DropoffAddressLine1)
+  console.log(passenger2DropoffAddressLine2)
+  
+  console.log(passenger3Name)
+  console.log(passenger3Phone)
+  console.log(passenger3NumberOfPassengers)
+  console.log(passenger3PickupAddressLine1)
+  console.log(passenger3PickupAddressLine2)
+  console.log(passenger3DropoffAddressLine1)
+  console.log(passenger3DropoffAddressLine2)
+
 }
 
 
