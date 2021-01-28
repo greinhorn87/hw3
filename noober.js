24297e6697a1195949e314745854eb602b238b1c
Garrett,Einhorn <garrett.einhorn@kellogg.northwestern.edu>
2021-01-27 22:51:31 -0600

    trying to define pass 2

--- noober.js
+++ noober.js
@@ -30,32 +30,61 @@ let passenger3 = ride[2]
 
 
 
+// if (ride.length == 1 || ride.length == 2 || ride.length == 3) {
+//   passenger1Name = passenger1.passengerDetails.first + ' ' + passenger1.passengerDetails.last
+//   passenger1Phone = passenger1.passengerDetails.phoneNumber
+//   passenger1NumberOfPassengers = passenger1.passengerDetails.numberOfPassengers
+//   passenger1PickupAddressLine1 = passenger1.pickupLocation.address
+//   passenger1PickupAddressLine2 = passenger1.pickupLocation.city + " " + passenger1.pickupLocation.state + " " + passenger1.pickupLocation.zip
+//   passenger1DropoffAddressLine1 = passenger1.dropoffLocation.address
+//   passenger1DropoffAddressLine2 = passenger1.dropoffLocation.city + " " + passenger1.dropoffLocation.state + ' ' +passenger1.dropoffLocation.zip 
+// } 
+// else if (ride.length == 2 || ride.length == 3) {
+//   passenger2Name = passenger2.passengerDetails.first + ' ' + passenger2.passengerDetails.last
+//   passenger2Phone = passenger2.passengerDetails.phoneNumber
+//   passenger2NumberOfPassengers = passenger2.passengerDetails.numberOfPassengers
+//   passenger2PickupAddressLine1 = passenger2.pickupLocation.address
+//   passenger2PickupAddressLine2 = passenger2.pickupLocation.city + " " + passenger2.pickupLocation.state + " " + passenger2.pickupLocation.zip
+//   passenger2DropoffAddressLine1 = passenger2.dropoffLocation.address
+//   passenger2DropoffAddressLine2 = passenger2.dropoffLocation.city + " " + passenger2.dropoffLocation.state + ' ' +passenger2.dropoffLocation.zip 
+// }
+// else if (ride.length == 3) {
+//   passenger3Name = passenger3.passengerDetails.first + ' ' + passenger3.passengerDetails.last
+//   passenger3Phone = passenger3.passengerDetails.phoneNumber
+//   passenger3NumberOfPassengers = passenger3.passengerDetails.numberOfPassengers
+//   passenger3PickupAddressLine1 = passenger3.pickupLocation.address
+//   passenger3PickupAddressLine2 = passenger3.pickupLocation.city + " " + passenger3.pickupLocation.state + " " + passenger3.pickupLocation.zip
+//   passenger3DropoffAddressLine1 = passenger3.dropoffLocation.address
+//   passenger3DropoffAddressLine2 = passenger3.dropoffLocation.city + " " + passenger3.dropoffLocation.state + ' ' +passenger3.dropoffLocation.zip 
+// }
+
+
 if (ride.length == 1 || ride.length == 2 || ride.length == 3) {
-  passenger1Name = passenger1.passengerDetails.first + ' ' + passenger1.passengerDetails.last
-  passenger1Phone = passenger1.passengerDetails.phoneNumber
-  passenger1NumberOfPassengers = passenger1.passengerDetails.numberOfPassengers
-  passenger1PickupAddressLine1 = passenger1.pickupLocation.address
-  passenger1PickupAddressLine2 = passenger1.pickupLocation.city + " " + passenger1.pickupLocation.state + " " + passenger1.pickupLocation.zip
-  passenger1DropoffAddressLine1 = passenger1.dropoffLocation.address
-  passenger1DropoffAddressLine2 = passenger1.dropoffLocation.city + " " + passenger1.dropoffLocation.state + ' ' +passenger1.dropoffLocation.zip 
+  passenger1Name = passenger.passengerDetails.first + ' ' + passenger.passengerDetails.last
+  passenger1Phone = passenger.passengerDetails.phoneNumber
+  passenger1NumberOfPassengers = passenger.passengerDetails.numberOfPassengers
+  passenger1PickupAddressLine1 = passenger.pickupLocation.address
+  passenger1PickupAddressLine2 = passenger.pickupLocation.city + " " + passenger.pickupLocation.state + " " + passenger.pickupLocation.zip
+  passenger1DropoffAddressLine1 = passenger.dropoffLocation.address
+  passenger1DropoffAddressLine2 = passenger.dropoffLocation.city + " " + passenger.dropoffLocation.state + ' ' +passenger.dropoffLocation.zip 
 } 
 else if (ride.length == 2 || ride.length == 3) {
-  passenger2Name = passenger2.passengerDetails.first + ' ' + passenger2.passengerDetails.last
-  passenger2Phone = passenger2.passengerDetails.phoneNumber
-  passenger2NumberOfPassengers = passenger2.passengerDetails.numberOfPassengers
-  passenger2PickupAddressLine1 = passenger2.pickupLocation.address
-  passenger2PickupAddressLine2 = passenger2.pickupLocation.city + " " + passenger2.pickupLocation.state + " " + passenger2.pickupLocation.zip
-  passenger2DropoffAddressLine1 = passenger2.dropoffLocation.address
-  passenger2DropoffAddressLine2 = passenger2.dropoffLocation.city + " " + passenger2.dropoffLocation.state + ' ' +passenger2.dropoffLocation.zip 
+  passenger2Name = passenger.passengerDetails.first + ' ' + passenger.passengerDetails.last
+  passenger2Phone = passenger.passengerDetails.phoneNumber
+  passenger2NumberOfPassengers = passenger.passengerDetails.numberOfPassengers
+  passenger2PickupAddressLine1 = passenger.pickupLocation.address
+  passenger2PickupAddressLine2 = passenger.pickupLocation.city + " " + passenger.pickupLocation.state + " " + passenger.pickupLocation.zip
+  passenger2DropoffAddressLine1 = passenger.dropoffLocation.address
+  passenger2DropoffAddressLine2 = passenger.dropoffLocation.city + " " + passenger.dropoffLocation.state + ' ' +passenger.dropoffLocation.zip 
 }
 else if (ride.length == 3) {
-  passenger3Name = passenger3.passengerDetails.first + ' ' + passenger3.passengerDetails.last
-  passenger3Phone = passenger3.passengerDetails.phoneNumber
-  passenger3NumberOfPassengers = passenger3.passengerDetails.numberOfPassengers
-  passenger3PickupAddressLine1 = passenger3.pickupLocation.address
-  passenger3PickupAddressLine2 = passenger3.pickupLocation.city + " " + passenger3.pickupLocation.state + " " + passenger3.pickupLocation.zip
-  passenger3DropoffAddressLine1 = passenger3.dropoffLocation.address
-  passenger3DropoffAddressLine2 = passenger3.dropoffLocation.city + " " + passenger3.dropoffLocation.state + ' ' +passenger3.dropoffLocation.zip 
+  passenger3Name = passenger.passengerDetails.first + ' ' + passenger.passengerDetails.last
+  passenger3Phone = passenger.passengerDetails.phoneNumber
+  passenger3NumberOfPassengers = passenger.passengerDetails.numberOfPassengers
+  passenger3PickupAddressLine1 = passenger.pickupLocation.address
+  passenger3PickupAddressLine2 = passenger.pickupLocation.city + " " + passenger.pickupLocation.state + " " + passenger.pickupLocation.zip
+  passenger3DropoffAddressLine1 = passenger.dropoffLocation.address
+  passenger3DropoffAddressLine2 = passenger.dropoffLocation.city + " " + passenger.dropoffLocation.state + ' ' +passenger.dropoffLocation.zip 
 }
 
 console.log(passenger1Name)
