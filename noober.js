b5ab54975be8a9a3aa8a957436aca777a7adfd30
Garrett,Einhorn <garrett.einhorn@kellogg.northwestern.edu>
2021-01-27 22:29:33 -0600

    stuck

--- noober.js
+++ noober.js
@@ -38,8 +38,8 @@ if (ride.length == 1) {
   passenger1PickupAddressLine2 = passenger1.pickupLocation.city + " " + passenger1.pickupLocation.state + " " + passenger1.pickupLocation.zip
   passenger1DropoffAddressLine1 = passenger1.dropoffLocation.address
   passenger1DropoffAddressLine2 = passenger1.dropoffLocation.city + " " + passenger1.dropoffLocation.state + ' ' +passenger1.dropoffLocation.zip 
-
-} else if (ride.length == 2) {
+} 
+else if (ride.length == 2) {
   passenger2Name = passenger2.passengerDetails.first + ' ' + passenger2.passengerDetails.last
   passenger2Phone = passenger2.passengerDetails.phoneNumber
   passenger2NumberOfPassengers = passenger2.passengerDetails.numberOfPassengers
@@ -47,8 +47,8 @@ if (ride.length == 1) {
   passenger2PickupAddressLine2 = passenger2.pickupLocation.city + " " + passenger2.pickupLocation.state + " " + passenger2.pickupLocation.zip
   passenger2DropoffAddressLine1 = passenger2.dropoffLocation.address
   passenger2DropoffAddressLine2 = passenger2.dropoffLocation.city + " " + passenger2.dropoffLocation.state + ' ' +passenger2.dropoffLocation.zip 
-
-}else if (ride.length == 3) {
+}
+else if (ride.length == 3) {
   passenger3Name = passenger3.passengerDetails.first + ' ' + passenger3.passengerDetails.last
   passenger3Phone = passenger3.passengerDetails.phoneNumber
   passenger3NumberOfPassengers = passenger3.passengerDetails.numberOfPassengers
